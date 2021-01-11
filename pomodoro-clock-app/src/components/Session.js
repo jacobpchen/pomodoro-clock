48464e7d949ae12885a78765649b285afb036622
Jacob Chen <jacobpchen@gmail.com>
2021-01-10 22:23:52 -0500

    added disabled buttons for session length

--- pomodoro-clock-app/src/components/Session.js
+++ pomodoro-clock-app/src/components/Session.js
@@ -18,11 +18,13 @@ function SessionLength(props) {
     return (
         <div><h4 className="text-center my-3">Session Length</h4>
             <section className="d-flex mx-5 align-items-center">
-                <button disabled={props.isPlay === true ? "disabled" : ""}
+                <button
+                    disabled={props.isPlay === true ? "disabled" : ""}
                     onClick={decreaseSession} type="button"
                     className="btn btn-primary mx-3">Down</button>
                 <p className="mx-2">{props.sessionLength}</p>
-                <button disabled={props.isPlay === true ? "disabled" : ""}
+                <button
+                    disabled={props.isPlay === true ? "disabled" : ""}
                     onClick={increaseSession} type="button"
                     className="btn btn-primary mx-3">Up</button>
 
